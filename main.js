79d260d4fad5623e2bd4bbe6e98574c23a3a0aba
Ambar2010 <89517719+Ambar2010@users.noreply.github.com>
2022-04-27 15:51:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function preload() {
 }
  
 function setup() {
-  canvas = createCanvas(600, 450);
+  canvas = createCanvas(640, 480);
   canvas.position(110, 250);
   video = createCapture(VIDEO);
   video.hide();
@@ -12,6 +12,20 @@ function setup() {
  
 function draw() {
   image(video, 180, 130, 250, 200);
+
+ fill(255,0,0);
+ stroke(255,0,0);
+ circle(50,50,80);
+ circle(50,430,80);
+ circle(590,50,80);
+ circle(590,430,80);
+
+ fill(0,0,200);
+ stroke(0,0,200);
+ rect(90,40,460,20);
+ rect(90,420,460,20);
+ rect(40,90,20,300);
+ rect(580,90,20,300);
 }
  
 function take_snapshot(){    
